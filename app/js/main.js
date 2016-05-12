76424b76285e153c33951baad4bc99faf34e0fce
Andrew Faircloth <andrewkfaircloth@gmail.com>
2016-05-12 11:23:01 -0400

    file upload working

--- app/js/main.js
+++ app/js/main.js
@@ -137,26 +137,69 @@ var LoadPlayersController = function LoadPlayersController($scope, FangraphsServ
 
   var vm = this;
 
+  // view model functions
   vm.loadPlayers = loadPlayers;
+  vm.submitData = submitData;
+
+  // data
+  vm.positions = ['P', 'C', '1B', '2B', 'SS', '3B', 'OF'];
 
   console.log(FangraphsService);
 
-  function loadPlayers() {
+  vm.dataArray = [];
+
+  function loadPlayers(position) {
     console.log('file import called');
     var fileField = document.getElementById('playerImport');
     console.log(fileField);
-
     var content = $scope.csv.content;
-    console.log('content:   ', content);
 
-    var stringify = JSON.stringify(content);
-    console.log('stringify:   ', stringify);
-    // let fileObj = fileField.files[0];
-    // console.log(fileObj);
-    // FangraphsService.uploadFile(fileObj);
-    // .then( (res) => {
-    //   console.log(res);
-    // });
+    if (content) {
+      buildData(content, position);
+    } else {
+      alert('There was an error importing the data. Please try again.');
+    }
+  }
+
+  function buildData(content, position) {
+    var array = content.split(position + ',');
+    array.shift();
+    array.forEach(function (player) {
+      var playerArray = player.split(',');
+      var playerObj = {
+        position: position,
+        fgId: playerArray[0],
+        firstName: playerArray[1],
+        lastName: playerArray[2].substring(0, playerArray[2].length - 1)
+      };
+      vm.dataArray.push(playerObj);
+    });
+
+    if (array) {
+      vm.loaded = true;
+    }
+
+    setTimeout(function () {
+      showResult();
+    }, 3000);
+  }
+
+  function showResult() {
+    vm.loaded = true;
+    if (vm.dataArray.length > 1) {
+      console.log(vm.dataArray);
+    } else {
+      alert('There was an error. Please try again!');
+    }
+  }
+
+  function submitData() {
+    console.log('data to submit to database: ', vm.dataArray);
+    vm.dataArray.forEach(function (player) {
+      FangraphsService.addPlayer(player).then(function (res) {
+        console.log('server response: ', res);
+      });
+    });
   }
 };
 
--- js/app-fangraphs/controllers/load-players.controller.js
+++ js/app-fangraphs/controllers/load-players.controller.js
@@ -2,29 +2,70 @@ let LoadPlayersController = function($scope, FangraphsService) {
   
   let vm = this;
 
+  // view model functions
   vm.loadPlayers = loadPlayers;
+  vm.submitData  = submitData;
 
+  // data
+  vm.positions    = ['P', 'C', '1B', '2B', 'SS', '3B', 'OF'];
 
   console.log(FangraphsService);
 
+  vm.dataArray = [];
 
-  function loadPlayers () {
+
+  function loadPlayers (position) {
     console.log('file import called');
     let fileField = document.getElementById('playerImport');
     console.log(fileField);
-
-
     let content = $scope.csv.content; 
-    console.log('content:   ', content);
+    
+    if(content) {
+      buildData(content, position);
+    } else {
+      alert('There was an error importing the data. Please try again.');
+    }
+  }
 
-    let stringify = JSON.stringify(content);
-    console.log('stringify:   ', stringify);
-    // let fileObj = fileField.files[0];
-    // console.log(fileObj);
-    // FangraphsService.uploadFile(fileObj);
-    // .then( (res) => {
-    //   console.log(res);
-    // });
+  function buildData(content, position) {
+    let array = content.split(position + ',');
+    array.shift();
+    array.forEach( function (player) {
+      let playerArray = player.split(',');
+      let playerObj = {
+        position: position,
+        fgId: playerArray[0],
+        firstName: playerArray[1],
+        lastName: playerArray[2].substring(0, playerArray[2].length - 1)
+      };
+      vm.dataArray.push(playerObj);
+    });
+
+    if (array) {
+      vm.loaded = true;
+    }
+
+    setTimeout(function() {
+      showResult();
+    }, 3000);
+  }
+
+  function showResult () {
+    vm.loaded = true;
+    if (vm.dataArray.length > 1) {
+      console.log(vm.dataArray);
+    } else {
+      alert('There was an error. Please try again!');
+    }
+  }
+
+  function submitData () {
+    console.log('data to submit to database: ', vm.dataArray);
+    vm.dataArray.forEach(function(player) {
+      FangraphsService.addPlayer(player).then( (res) => {
+        console.log('server response: ', res);
+      });
+    });
   }
 };
 
